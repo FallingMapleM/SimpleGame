174694442697c33e903b1334c649d8f75c5e5900
EthanM <459166889@qq.com>
2024-06-02 02:17:33 +0800

    更新场景

--- Assets/Scripts/LunaController.cs
+++ Assets/Scripts/LunaController.cs
@@ -5,17 +5,35 @@ using UnityEngine.UIElements;
 
 public class LunaController : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2d;
+    public float moveSpeed = 15;
+    public int maxHealth = 5;
+    private int currentHealth;
+    
    // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponentInParent<Rigidbody2D>();
+        currentHealth = maxHealth;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + moveSpeed * vertical * Time.deltaTime;
+        // transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+    private void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth+"/"+maxHealth);
     }
 }
