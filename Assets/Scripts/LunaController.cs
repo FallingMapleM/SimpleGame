b152c43853c1a87a113dece9d6b6db13bc6ed97e
EthanM <11730186+ethanm@user.noreply.gitee.com>
2024-06-02 01:13:37 +0800

    更新

--- Assets/Scripts/LunaController.cs
+++ Assets/Scripts/LunaController.cs
@@ -5,14 +5,7 @@ using UnityEngine.UIElements;
 
 public class LunaController : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2d;
-    private Animator animator;
-    private Vector2 move;
-    public float moveScale;
-    public float moveSpeed;
-    private Vector2 lookDirection = new Vector2 (1, 0);
-
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
         
@@ -21,20 +14,8 @@ public class LunaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ÕÊº“ ‰»Î
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("vertical");
-        move = new Vector2 (horizontal, vertical);
-
-
-
         Vector2 position = transform.position;
-        position = position + moveSpeed * move * Time.fixedDeltaTime;
-        rigidbody2d.MovePosition(position);
-
-        //transform.position = position;
-
-        
-        
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
